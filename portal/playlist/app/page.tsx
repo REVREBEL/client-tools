4ba68f08ba5a20b668da19d67f022b6c830d4ef3
Gary Stringham <141124094+RR-Gary-Stringham@users.noreply.github.com>
2026-09-07 13:59:21 -0700

    load workspace setup alongside playlist data

--- portal/playlist/app/page.tsx
+++ portal/playlist/app/page.tsx
@@ -1,9 +1,9 @@
 import PlaylistLayout from "../Components/Playlist/PlaylistLayout";
-import { loadPlaylistData } from "./lib/google-sheets";
+import { loadPlaylistData, loadWorkspaceSetupData } from "./lib/google-sheets";
 
 export const dynamic = "force-dynamic";
 
 export default async function PlaylistPage() {
-  const data = await loadPlaylistData();
-  return <PlaylistLayout data={data} />;
+  const [data, setup] = await Promise.all([loadPlaylistData(), loadWorkspaceSetupData()]);
+  return <PlaylistLayout data={data} setup={setup} />;
 }
