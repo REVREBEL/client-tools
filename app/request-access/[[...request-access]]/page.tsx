fb240ffba896f213b2f613d26ace320c364b8317
Gary Stringham <141124094+RR-Gary-Stringham@users.noreply.github.com>
2026-09-07 21:35:39 -0700

    Use public portal URL on Clerk waitlist

--- app/request-access/[[...request-access]]/page.tsx
+++ app/request-access/[[...request-access]]/page.tsx
@@ -1,15 +1,14 @@
 import { Waitlist } from "@clerk/nextjs";
 import AuthShell from "../../auth-shell";
 import { clerkAuthAppearance } from "../../clerk-auth";
+import { portalPublicUrl } from "../../lib/portal-urls";
 
 export const dynamic = "force-dynamic";
-
-const BASE_PATH = process.env.NEXT_PUBLIC_BASE_PATH ?? "";
 
 export default function RequestAccessPage() {
   return (
     <AuthShell>
-      <Waitlist appearance={clerkAuthAppearance} signInUrl={`${BASE_PATH}/sign-in`} />
+      <Waitlist appearance={clerkAuthAppearance} signInUrl={portalPublicUrl("/sign-in")} />
     </AuthShell>
   );
 }
