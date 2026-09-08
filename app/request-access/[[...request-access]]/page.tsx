ddc9b578ef9b8ecb36af85ede1b0331f3759d359
Gary Stringham <141124094+RR-Gary-Stringham@users.noreply.github.com>
2026-09-08 02:25:05 -0700

    Use Clerk sign-in URL environment variable for waitlist

--- app/request-access/[[...request-access]]/page.tsx
+++ app/request-access/[[...request-access]]/page.tsx
@@ -1,14 +1,16 @@
 import { Waitlist } from "@clerk/nextjs";
 import AuthShell from "../../auth-shell";
 import { clerkAuthAppearance } from "../../clerk-auth";
-import { portalPublicUrl } from "../../lib/portal-urls";
+import { portalPath } from "../../lib/portal-urls";
 
 export const dynamic = "force-dynamic";
+
+const SIGN_IN_URL = process.env.NEXT_PUBLIC_CLERK_SIGN_IN_URL || portalPath("/sign-in");
 
 export default function RequestAccessPage() {
   return (
     <AuthShell>
-      <Waitlist appearance={clerkAuthAppearance} signInUrl={portalPublicUrl("/sign-in")} />
+      <Waitlist appearance={clerkAuthAppearance} signInUrl={SIGN_IN_URL} />
     </AuthShell>
   );
 }
