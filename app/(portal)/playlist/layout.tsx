752bf493ab51bd269b0fd8ac1f0717610ff4e48f
Gary Stringham <141124094+RR-Gary-Stringham@users.noreply.github.com>
2026-09-07 19:38:14 -0700

    load restored playlist dashboard styles

--- app/(portal)/playlist/layout.tsx
+++ app/(portal)/playlist/layout.tsx
@@ -1,4 +1,5 @@
 import "../../../portal/playlist/app/globals.css";
+import "../../../portal/playlist/app/dashboard.css";
 
 export default function PlaylistLayout({ children }: { children: React.ReactNode }) {
   return <div data-client-tool="playlist">{children}</div>;
