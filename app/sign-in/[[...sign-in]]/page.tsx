3ae643a1afd39eee8088b9bd6ecd7dd0935d2cf8
Gary Stringham <141124094+RR-Gary-Stringham@users.noreply.github.com>
2026-09-07 21:35:34 -0700

    Use public portal URLs on Clerk sign-in

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,19 +1,18 @@
 import { SignIn } from "@clerk/nextjs";
 import AuthShell from "../../auth-shell";
 import { clerkAuthAppearance } from "../../clerk-auth";
+import { portalPath, portalPublicUrl } from "../../lib/portal-urls";
 
 export const dynamic = "force-dynamic";
-
-const BASE_PATH = process.env.NEXT_PUBLIC_BASE_PATH ?? "";
 
 export default function SignInPage() {
   return (
     <AuthShell>
       <SignIn
         appearance={clerkAuthAppearance}
-        path={`${BASE_PATH}/sign-in`}
+        path={portalPath("/sign-in")}
         routing="path"
-        waitlistUrl={`${BASE_PATH}/request-access`}
+        waitlistUrl={portalPublicUrl("/request-access")}
         withSignUp={false}
       />
     </AuthShell>
